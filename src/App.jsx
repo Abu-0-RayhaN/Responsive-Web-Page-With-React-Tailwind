97d94dcc9b91c8f1396b46a7a89552e2cd21f299
Abu-0-RayhaN <rayhan844625@gmail.com>
2023-05-04 23:01:46 +0600

    Hero and Stats

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,45 @@
 // import React from 'react';
 // import { Navbar,Hero,Stats,Business,Billing,CardDeal,Testimonials,Clients,Cta, Footer  } from "./components"
-import { Navbar,Hero,Stats,Business,Billing,CardDeal,Testimonials,Clients, Cta,Footer } from "./components"
-import styles from "./style"
+import {
+  Navbar,
+  Hero,
+  Stats,
+  Business,
+  Billing,
+  CardDeal,
+  Testimonials,
+  Clients,
+  Cta,
+  Footer,
+} from "./components";
+import styles from "./style";
 const App = () => {
   return (
     <div className="bg-primary w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
-          <Navbar/>
+          <Navbar />
         </div>
       </div>
       <div className={`bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          <Hero/>
+          <Hero />
         </div>
       </div>
       <div className={`bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          <Stats/>
-          <Business/> 
-          <Billing/> 
-          <CardDeal/> 
-          <Testimonials/> 
-          <Clients/> 
-          <Cta/> 
-          <Footer/> 
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <Cta />
+          <Footer />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,11 +1,26 @@
 // import React from 'react'
-
+import { stats } from "../constants";
+import styles from "../style";
 const Stats = () => {
   return (
-    <div>
-      <p>Stats</p>
-    </div>
-  )
-}
+    <section
+      className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}
+    >
+      {stats.map((stats) => (
+        <div
+          className={`flex-1 flex justify-start items-center flex-row m-3`}
+          key={stats.id}
+        >
+          <h4 className=" text-white font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading[53px] leading-[43px]">
+            {stats.value}
+          </h4>
+          <p className="font-poppins font-normal xs:text-[20px] text-[15px] xs:leading[26px] leading-[21px] text-gradient uppercase ">
+            {stats.title}
+          </p>
+        </div>
+      ))}
+    </section>
+  );
+};
 
-export default Stats
+export default Stats;
